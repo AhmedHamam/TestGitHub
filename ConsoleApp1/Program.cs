b8b4a8578679c2d7f53dd0334e64e021d8909c5c
Ahmed Hamam <ahmedhamam50@gmail.com>
2023-08-12 15:52:21 +0300

    fix merge

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("welcome to git hub world");
+//Taha changes 
+Console.WriteLine("test with only one branch ");
